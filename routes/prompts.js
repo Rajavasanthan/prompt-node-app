6629e380e13037849b263bfe19b5fa6b1990dc6e
rajavasanthan <rajavasanthan@gmail.com>
2026-01-22 20:03:06 +0530

    change

--- routes/prompts.js
+++ routes/prompts.js
@@ -1,32 +1,50 @@
 var express = require("express");
 const { Prompt } = require("../model/prompt");
 var router = express.Router();
+const { ObjectId } = require("mongodb");
 
 /* GET home page. */
 router.get("/search-prompt", async function (req, res, next) {
   try {
-    let query = [
-      {
-        $match: {},
-      }
-    ];
+    let pipeline = [];
     if (req.query.search) {
-      query = [
-      {
+      pipeline.push({
         $search: {
           index: "title_search",
           text: {
             query: req.query.search,
-            path: {
-              wildcard: "*",
-            },
+            path: "title",
           },
         },
+      });
+    }
+
+    // 2️⃣ Mandatory filter (always applied)
+    pipeline.push({
+      $match: {},
+    });
+
+    // 3️⃣ Populate author
+    pipeline.push(
+      {
+        $lookup: {
+          from: "users",
+          localField: "author_id",
+          foreignField: "_id",
+          as: "author",
+        },
       },
-    ]
-  }
+      {
+        $unwind: {
+          path: "$author",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+    );
+    console.log(JSON.stringify(pipeline));
+    const prompts = await Prompt.aggregate(pipeline);
     // const prompts = await Prompt.find(query).populate("author_id");
-    const prompts = await Prompt.aggregate(query);
+    // const prompts = await Prompt.aggregate(query);
     return res.status(200).json({
       message: "Prompt fetched successfully",
       prompts,
@@ -41,34 +59,45 @@ router.get("/search-prompt", async function (req, res, next) {
 
 router.get("/get-my-prompt", async function (req, res, next) {
   try {
-    let query = [
-      {
-        $match: {
-          author_id: req.user._id
-        },
-      }
-    ];
+    let pipeline = [];
     if (req.query.search) {
-      query = [
-      {
+      pipeline.push({
         $search: {
           index: "title_search",
           text: {
             query: req.query.search,
-            path: {
-              wildcard: "*",
-            },
+            path: "title",
           },
+        },
+      });
+    }
+
+    // 2️⃣ Mandatory filter (always applied)
+    pipeline.push({
+      $match: {
+        author_id: new ObjectId(req.user._id),
+      },
+    });
+
+    // 3️⃣ Populate author
+    pipeline.push(
+      {
+        $lookup: {
+          from: "users",
+          localField: "author_id",
+          foreignField: "_id",
+          as: "author",
         },
       },
       {
-        $match: {
-          author_id: req.user._id
+        $unwind: {
+          path: "$author",
+          preserveNullAndEmptyArrays: true,
         },
-      }
-    ]
-    }
-    const prompts = await Prompt.aggregate(query)
+      },
+    );
+    console.log(JSON.stringify(pipeline));
+    const prompts = await Prompt.aggregate(pipeline);
     // const prompts = await Prompt.find(query).populate("author_id");
     return res.status(200).json({
       message: "Prompt fetched successfully",
